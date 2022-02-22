126e155952db4e3531d40f4ee5b95e2ea8e9db22
luccavp12 <lucca12vp@gmail.com>
2022-02-21 22:22:07 -0800

    Import and organize manifest data

--- hello.py
+++ hello.py
@@ -1,11 +1,10 @@
 from unicodedata import name
 from flask import Flask
 from flask import render_template
+import re
 import json
 
 app = Flask(__name__)
-
-@app.route("/")
 
 @app.route("/")
 def content():
@@ -19,14 +18,15 @@ def content():
             line = line.strip() # remove the leading/trailing white spaces
             if line:
                 d = {}
-                data = [item.strip() for item in line.split(',(?![^\[]*])')]
-                # print(data)
+                data = [item.strip() for item in re.split(',(?![^\[]*])', line)]
                 for index, elem in enumerate(data):
-                    print(index)
-                    print(data[index])
-                    # d[columns[index]] = data[index]
+                    d[columns[index]] = data[index]
             infoList.append(d)
     return render_template('hello.html', text = infoList)
+
+@app.route("/operations")
+def content():
+    return render_template('operations.html')
 
 
 # def hello_world(name=None):
