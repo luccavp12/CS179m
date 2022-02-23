d12fad5b4d3fd158b346c5c20a645e29a4934e62
luccavp12 <lucca12vp@gmail.com>
2022-02-22 18:55:26 -0800

    Allow user to select their own manifest file

--- hello.py
+++ hello.py
@@ -1,29 +1,31 @@
+from crypt import methods
 from unicodedata import name
-from flask import Flask
-from flask import render_template
+from flask import Flask, url_for, redirect, request, render_template
 import re
 import json
+import easygui
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return render_template('home.html')
-    
 
 @app.route("/operations")
 def operations():
-    with open('Manifests/ShipCase5.txt', mode = 'r', encoding= 'utf-8-sig') as f:
-        lines = f.readlines() # list containing lines of file
-        columns = ['position', 'weight', 'description']
+    manifestPath = easygui.fileopenbox()    # Prompts the user with the file explorer to choose a manifest
+    print(manifestPath)                     
+    with open(manifestPath, mode = 'r', encoding= 'utf-8-sig') as f:    # Uses manifest path to open file
+        lines = f.readlines()   # List containing lines of file
+        columns = ['position', 'weight', 'description'] # Creates a list of column names
         infoList = []
 
         i = 1
         for line in lines:
-            line = line.strip() # remove the leading/trailing white spaces
+            line = line.strip() # Remove the leading/trailing white spaces
             if line:
                 d = {}
-                data = [item.strip() for item in re.split(',(?![^\[]*])', line)]
+                data = [item.strip() for item in re.split(',(?![^\[]*])', line)]    # Uses regex to split the manifest data by the correct commas
                 for index, elem in enumerate(data):
                     d[columns[index]] = data[index]
             infoList.append(d)
