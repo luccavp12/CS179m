07efc69503f7ee46b66ae88648f51ec041ccca96
luccavp12 <lucca12vp@gmail.com>
2022-02-24 11:57:35 -0800

    Add finish button to operations and give each container a unique ID

--- hello.py
+++ hello.py
@@ -41,16 +41,17 @@ def home():
 
 @app.route("/operations")
 def operations():
-    manifestPath = easygui.fileopenbox()    # Prompts the user with the file explorer to choose a manifest
-    print(manifestPath)                     
-    with open(manifestPath, mode = 'r', encoding= 'utf-8-sig') as f:    # Uses manifest path to open file
-        lines = f.readlines()   # List containing lines of file
-        columns = ['position', 'weight', 'description'] # Creates a list of column names
+    manifestPath = easygui.fileopenbox()                                            # Prompts the user with the file explorer to choose a manifest
+    print(manifestPath)
+    # manifestPath = "Manifests/ShipCase5.txt"                     
+    with open(manifestPath, mode = 'r', encoding= 'utf-8-sig') as f:                # Uses manifest path to open file
+        lines = f.readlines()                                                       # List containing lines of file
+        columns = ['position', 'weight', 'description']                             # Creates a list of column names
         infoList = []
 
         i = 1
         for line in lines:
-            line = line.strip() # Remove the leading/trailing white spaces
+            line = line.strip()                                                     # Remove the leading/trailing white spaces
             if line:
                 d = {}
                 data = [item.strip() for item in re.split(',(?![^\[]*])', line)]    # Uses regex to split the manifest data by the correct commas
