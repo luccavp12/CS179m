d750586658502999f335c6fa2fb47ceee39b3e01
rsohi001 <rsohi001@ucr.edu>
2022-03-05 14:46:48 -0800

    Finished base cases for balance.py MUST ADD testcase of one overweight container

--- balance.py
+++ balance.py
@@ -3,9 +3,60 @@ import json
 sampleJson = 0 #Pass in JSON here
 
 def balance(sampleJson):
-    x
+    right_weight, left_weight, maxWeight, max_Index, isBalanced, total = getWeights(sampleJson)
+    balance_ratio = left_weight/right_weight
+    best_balance = 100000.0 #This value will hold the ratio closest to balanced
+    best_index = ""
+    curr_balance = 100000.0 #This value will hold the ratio of the containers we are currently considering swapping.
+    
+    if isBalanced == True: #When ship is balanced
+        print("The ship is already balanced!")
+        
+    while(balance_ratio > 1.1 or balance_ratio < .9):
+        if balance_ratio > 1.1: #Left side is heavier we need to move something from here to right side
+            
+            for i in range(8,0,-1):
+                for j in range(6,0,-1):
+                    left_cont = makeIndex(i,j) #left_cont is just the index so we can access the weight values
+                    new_left = left_weight - sampleJson[left_cont]["weight"]
+                    new_right = right_weight + sampleJson[left_cont]["weight"]
+                    curr_balance = new_left/new_right
+                    if curr_balance < 1.1 and curr_balance > 0.9:
+                        #MOVE left_cont to nearest rightside spot CODE MUST BREAK HERE SINCE WE BALANCED THE SHIP
+                    else:
+                        if abs(curr_balance-1) < abs(best_balance-1):
+                            best_balance = curr_balance
+                            best_index = left_cont
+                            
+            #If we made it here in our code that means that no move balanced the ship, so we will make the best move we can currently make.
+            #MOVE best_index to the nearest rightside spot, and continue the while loop.
+            
+        if balance_ratio < .9: #Right side is heavier we need to move something from here to left side.
+            
+            for i in range(8,0,-1):
+                for j in range(7,13):
+                    right_cont = makeIndex(i,j)
+                    new_right = right_weight - sampleJson[right_cont]["weight"]
+                    new_left = left_weight + sampleJson[right_cont]["weight"]
+                    curr_balance = new_left/new_right
+                    if curr_balance < 1.1 and curr_balance > 0.9:
+                        #Move right_cont to nearest leftside spot CODE MUST BREAK HERE SINCE WE BALANACED THE SHIP
+                    else:
+                        if abs(curr_balance-1) < abs(best_balance-1):
+                            best_balance = curr_balance
+                            best_index = right_cont
+                            
+            #If we made it here in our code that means that no move balanced the ship, so we will make the best move we can currently make.
+            #Move best_index to the nearest leftside spot, and continue the while loop.
+            
+        #After making some sort of change by either doing the right and left movement, we need to recalculate balance_ratio.
+        left_weight = getLeftWeight(sampleJson)
+        right_weight = getRightWeight(sampleJson)
+        balance_ratio = left_weight/right_weight
+#----------------------------------------------------------balance() ends here--------------------------------------------------------------#
+    
 
-#------------------Helper-Functions-------------------------#
+#----------------------------------------------------------Helper-Functions-----------------------------------------------------------------#
 def makeIndex(y, x):
     if y > 9:   # If the index if greater than 9, then we format as a double digit instead of appending a 0 to the end
         if x > 9: # Same situation as y
@@ -59,11 +110,11 @@ def getRightWeight(sampleJson): #This function will return as a int the entire w
             
     return total
 
-def getWeights(sampleJson): #This function will return 5 values, int right_weight, int left_weight, int maxWeight, string max_Index, and string heavy.
+def getWeights(sampleJson): #This function will return 5 values, int right_weight, int left_weight, int maxWeight, string max_Index, int total, and string heavy.
     total = 0
     maxWeight = 0
     max_Index = ""
-    heavySide = ""
+    isBalanced = False
     right_weight = getRightWeight(sampleJson)
     left_weight = getLeftWeight(sampleJson)
     
@@ -76,16 +127,10 @@ def getWeights(sampleJson): #This function will return 5 values, int right_weigh
                 maxWeight = currWeight
                 max_Index = index
     
-    if right_weight > left_weight:
-        heavy = "Right"   
+    if left_weight/right_weight < 1.1 and left_weight/right_weight > .9:
+        isBalanced = True  
     
-    if left_weight > left_weight:
-        heavy = "Left"
-        
-    if right_weight > left_weight:
-        heavy = "Balanced"      
-    
-    return right_weight, left_weight, maxWeight, max_Index, heavy
+    return right_weight, left_weight, maxWeight, max_Index, isBalanced, total
             
             
     
