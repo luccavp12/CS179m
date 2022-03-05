b82921df6d94770a7e294f1979a518a446b86ea1
rsohi001 <rsohi001@ucr.edu>
2022-03-05 12:58:34 -0800

    Added comments to balance.py to aid in group code

--- balance.py
+++ balance.py
@@ -12,28 +12,31 @@ def balance(sampleJson):
         right_heavy = False
     balance = max(l_weight/r_weight, r_weight/l_weight)
     if balance > 1.1:
-        if right_heavy is True:
-            weight_diff = r_weight-l_weight
-            for i in range(1,9):
-                for j in range(7,13):
-                    heavy_container = sampleJson[makeIndex(i,j)]["weight"]
-                    for y in range(1,9):
-                        for x in range(1,7):
-                            light_container = sampleJson[makeIndex(y,x)]["weight"]
-                            balance_level = (getRightWeight() - heavy_container + light_container) / (getLeftWeight() - light_container + heavy_container)
-                            if balance_level <= 1.1 and balance_level >= .9:
-                                #swap(makeIndex(i,j),makeIndex(y,x))
-        else:
-            weight_diff = l_weight-r_weight
-            for i in range(1,9):
-                for j in range(1,7):
-                    heavy_container = sampleJson[makeIndex(i,j)]["weight"]
-                    for y in range(1,9):
-                        for x in range(7,13):
-                            light_container = sampleJson[makeIndex(y,x)]["weight"]
-                            balance_level = (getLeftWeight() - heavy_container + light_container) / (getRightWeight() - light_container + heavy_container)
-                            if balance_level <= 1.1 and balance_level >= .9:
-                                #swap(makeIndex(i,j),makeIndex(y,x))
+        
+        # Attempt 1 at balancing function, check layer 1,
+        # if right_heavy is True:
+        #     weight_diff = r_weight-l_weight
+        #     for i in range(1,9):
+        #         for j in range(7,13):
+        #             heavy_container = sampleJson[makeIndex(i,j)]["weight"]
+        #             for y in range(1,9):
+        #                 for x in range(1,7):
+        #                     light_container = sampleJson[makeIndex(y,x)]["weight"]
+        #                     balance_level = (getRightWeight() - heavy_container + light_container) / (getLeftWeight() - light_container + heavy_container)
+        #                     if balance_level <= 1.1 and balance_level >= .9:
+        #                         #swap(makeIndex(i,j),makeIndex(y,x))
+        #                         #BREAK -- balance might need to be a while loop, after first run of swap, break so that we go back to top of loop
+        # else:
+        #     weight_diff = l_weight-r_weight
+        #     for i in range(1,9):
+        #         for j in range(1,7):
+        #             heavy_container = sampleJson[makeIndex(i,j)]["weight"]
+        #             for y in range(1,9):
+        #                 for x in range(7,13):
+        #                     light_container = sampleJson[makeIndex(y,x)]["weight"]
+        #                     balance_level = (getLeftWeight() - heavy_container + light_container) / (getRightWeight() - light_container + heavy_container)
+        #                     if balance_level <= 1.1 and balance_level >= .9:
+        #                         #swap(makeIndex(i,j),makeIndex(y,x))
 
 
 #------------------Helper-Functions-------------------------#
@@ -64,7 +67,7 @@ def ifLeftEmpty(sampleJson):
 
 def getLeftWeight(sampleJson):
     total = 0
-    for i in range(1):
+    for i in range(1): #Height
         for j in range(6):
             index = makeIndex(i+1,j+1)
             total += int(sampleJson[index]["weight"])
@@ -83,11 +86,40 @@ def ifRightEmpty(sampleJson):
 
 def getRightWeight(sampleJson):
     total = 0
-    for i in range(1):
+    for i in range(1): #Height
         for j in range(6):
-        if j > 2:
             index = makeIndex(i+1,j+7)
-        else:
-            index = makeIndex(i+1,j+7)
-        total += int(sampleJson[index]["weight"])
+            total += int(sampleJson[index]["weight"])
+            
     return total
+
+def getWeights(sampleJson):
+    total = 0
+    maxWeight = 0
+    max_Index = ""
+    heavySide = ""
+    right_weight = getRightWeight(sampleJson)
+    left_weight = getLeftWeight(sampleJson)
+    
+    for i in range(8):
+        for j in range(12):
+            index = makeIndex(i+1,j+1)
+            currWeight = int(sampleJson[index]["weight"])
+            total += currWeight
+            if currWeight > maxWeight:
+                maxWeight = currWeight
+                max_Index = index
+    
+    if right_weight > left_weight:
+        heavy = "Right"   
+    
+    if left_weight > left_weight:
+        heavy = "Left"
+        
+    if right_weight > left_weight:
+        heavy = "Balanced"      
+    
+    return right_weight, left_weight, maxWeight, max_Index
+            
+            
+    
