e1aea167fb07163de12ab5d151434eae346c6381
rsohi001 <rsohi001@ucr.edu>
2022-03-05 13:01:55 -0800

    Saved code and added more comments

--- balance.py
+++ balance.py
@@ -41,8 +41,8 @@ def balance(sampleJson):
 
 #------------------Helper-Functions-------------------------#
 def makeIndex(y, x):
-    if y > 9:
-        if x > 9:
+    if y > 9:   # If the index if greater than 9, then we format as a double digit instead of appending a 0 to the end
+        if x > 9: # Same situation as y
             index = "["+ str(y) + "," + str(x) + "]"
         else:
             index = "["+ str(y) + ",0" + str(x) + "]"
@@ -53,11 +53,11 @@ def makeIndex(y, x):
             index = "[0"+ str(y) + ",0" + str(x) + "]"
     return index
 
-def checkDesc(y, x, sampleJson):
-    index = makeIndex(y,x)
+def checkDesc(y, x, sampleJson): #This function will take the y and x as well as the Json we are operating on and check the description of the package
+    index = makeIndex(y,x) 
     return sampleJson[index]["description"]
 
-def ifLeftEmpty(sampleJson):
+def ifLeftEmpty(sampleJson): # This function needs to be workshopped into finding the nearest empty space TO FIX
     for i in range(8):
         for j in range(6):
             index = makeIndex(i+1,j+1)
@@ -65,15 +65,7 @@ def ifLeftEmpty(sampleJson):
                 return index
     return False
 
-def getLeftWeight(sampleJson):
-    total = 0
-    for i in range(1): #Height
-        for j in range(6):
-            index = makeIndex(i+1,j+1)
-            total += int(sampleJson[index]["weight"])
-    return total
-
-def ifRightEmpty(sampleJson):
+def ifRightEmpty(sampleJson): # This function needs to be workshopped into finding the nearest empty space TO FIX
     for i in range(1):
         for j in range(6):
         if j > 2:
@@ -84,16 +76,24 @@ def ifRightEmpty(sampleJson):
             return index
     return False
 
-def getRightWeight(sampleJson):
+def getLeftWeight(sampleJson): #This function will return as a int the entire weight of the left hand side.
     total = 0
-    for i in range(1): #Height
+    for i in range(8): #Height
+        for j in range(6):
+            index = makeIndex(i+1,j+1)
+            total += int(sampleJson[index]["weight"])
+    return total
+
+def getRightWeight(sampleJson): #This function will return as a int the entire weight of the right hand side.
+    total = 0
+    for i in range(8): #Height
         for j in range(6):
             index = makeIndex(i+1,j+7)
             total += int(sampleJson[index]["weight"])
             
     return total
 
-def getWeights(sampleJson):
+def getWeights(sampleJson): #This function will return 5 values, int right_weight, int left_weight, int maxWeight, string max_Index, and string heavy.
     total = 0
     maxWeight = 0
     max_Index = ""
@@ -119,7 +119,7 @@ def getWeights(sampleJson):
     if right_weight > left_weight:
         heavy = "Balanced"      
     
-    return right_weight, left_weight, maxWeight, max_Index
+    return right_weight, left_weight, maxWeight, max_Index, heavy
             
             
     
